be518d9bfe8ef75254dbfc2048f181084c3d3616
geortron <george.trontsios@gmail.com>
2019-11-14 21:57:39 +0200

    commit

---
+++ build/precache-manifest.1cf00af1850ec7c76c6cd0af0a0f1ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9002c16bb87f4b652da0486899d192",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97a5311edfb051b25232",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "1254b00ce60808a30111",
+    "url": "/static/css/main.562cd718.chunk.css"
+  },
+  {
+    "revision": "97a5311edfb051b25232",
+    "url": "/static/js/2.e8feb55f.chunk.js"
+  },
+  {
+    "revision": "1254b00ce60808a30111",
+    "url": "/static/js/main.65e45631.chunk.js"
+  },
+  {
+    "revision": "f18cf89694a2c8853c62",
+    "url": "/static/js/runtime~main.faaba110.js"
+  }
+]);
